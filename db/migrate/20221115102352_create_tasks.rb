1f08d3a60a351a9b803a08b6c1e884d1ae6c661d
Emma ROSSO <emmarosso.pro@gmail.com>
2022-11-15 17:59:58 +0100

    tasks-manager

--- db/migrate/20221115102352_create_tasks.rb
+++ db/migrate/20221115102352_create_tasks.rb
@@ -2,10 +2,8 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :details
-      t.boolean :completed
-      t.string :default
-      t.string :false
+      t.text :description
+      t.boolean :completed, default: false
 
       t.timestamps
     end
